cdc07ed6b0212eff3b52d17517e4e0d63ae31207
Basil Baby <basilbaby112@gmail.com>
2022-07-12 17:49:51 +0530

    some changes

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @include('partials/__search')
 
-<a href="index.html" class="inline-block text-black ml-4 mb-4"
+<a href="/" class="inline-block text-black ml-4 mb-4"
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
